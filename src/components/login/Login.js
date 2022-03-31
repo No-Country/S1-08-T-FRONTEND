47a057b1adcfc1c0fa78b8de4040f38e1eb52de0
oliveravictor <ovictoriox@gmail.com>
2022-03-31 01:00:40 -0300

    add form

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+   <div>
+        <form>
+            <input type="text" placeholder="Usuario" name="user" minlength="4" maxlength="16" required />
+            <input type="password" placeholder="Clave" name="password" minlength="4" required />
+            <input type="password" placeholder="Confirmar clave" name="repassword" minlength="4" />
+            <button type="submit">Entrar</button>
+         </form>
     </div>
   );
 }
