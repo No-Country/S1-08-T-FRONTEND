d2090d7ddb02d9f3a3b9ac435691591de0e1114c
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2022-04-05 21:11:36 -0300

    commit
    Signed-off-by: MauricioRaulFerreyra <mauriciotauro82@hotmail.com>

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from './register.module.css'
 
-function Register () {
-  return <div>Register</div>
-}
+export default function Register () {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  const history = useNavigate()
 
-export default Register
+  async function handleSubmit (e) {
+    e.preventDefault()
+
+    try {
+      setError('')
+      setLoading(true)
+      history.push('/')
+    } catch {
+      setError('Fallo al crear la cuenta')
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    <div>
+      <section className={styles.login}>
+        <div className={styles.loginContainer}>
+          <h1>Registrate</h1>
+          {error && <h1>{error}</h1>}
+          <form onSubmit={handleSubmit}>
+            <input
+              type='email'
+              autoFocus
+              placeholder='Email'
+              required
+              ref={emailRef}
+            />
+
+            <input
+              type='password'
+              placeholder='Password'
+              required
+              ref={passwordRef}
+            />
+            <div className='btnContainer'>
+              <button type='submit' disabled={loading}>
+                Registrate
+              </button>
+              <br /> <br />
+              <p>
+                Ya tienes cuenta?{' '}
+                <Link to='/login'>
+                  <span>Inicia Sesión</span>
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  )
+}
