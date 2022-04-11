0130a1d1e5adcdc3141a8215705f33f538a655a3
nicobonder <nicobonder@hotmail.com>
2022-04-11 16:31:51 -0300

    working on upload videos

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -1,20 +1,21 @@
 import * as React from 'react';
+import './CreatePost.css'
+
 import Modal from '@mui/material/Modal';
 
-import { uploadImage } from '../../app/services/images';
-import { uploadVideo } from '../../app/services/images';
 import { useState, useEffect } from 'react';
 import { useSelector } from "react-redux";
 import { useCreatePostMutation } from "../../app/services/posts";
-import { useUploadPostsImageMutation } from "../../app/services/images";
-import { useUploadPostsVideoMutation } from "../../app/services/images"; 
-import './CreatePost.css';
-import { FilePond, File, registerPlugin } from 'react-filepond';
+import { uploadImage, uploadVideo} from "../../app/services/images";
+
+//FilePond library
+import { FilePond, registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
 
 const style = {
   position: 'absolute',
@@ -35,16 +36,17 @@ export default function CreatePost() {
 
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [category, setCategory] = useState("");
-  const [image, setImage] = useState(""); //para que sea una array, deberia tener[]
-  const [imageUrl, setImageUrl] = useState("");
-  const [video, setVideo] = useState(""); //para que sea una array, deberia tener[]
-  const [videoUrl, setVideoUrl] = useState("");
+  const [category, setCategory] = useState(null);
+  const [image, setImage] = useState(null); //para que sea una array, deberia tener[]
+  const [imageUrl, setImageUrl] = useState(null);
+  const [video, setVideo] = useState(null); //para que sea una array, deberia tener[]
+  const [videoUrl, setVideoUrl] = useState(null);
+
   const { user, isAuthenticated } = useSelector((state) => state.authUsers); //solo puede postear un usuario logueado
+
   const [createPost] = useCreatePostMutation();
-  const [createImage] = useUploadPostsImageMutation();
-  const [createVideo] = useUploadPostsVideoMutation();
  
+
   useEffect(() => {
     async function getImageUrl() {
       if (image) {
@@ -69,42 +71,58 @@ export default function CreatePost() {
       }
     }
     getVideoUrl();
-  }, [image]);
+  }, [video]);
 
-  
+
   const handleChange = (e) => {
-      const { name, value } = e.target;
-      if (name === "title") {
-          setTitle(value);
-      } else if (name === "description"){
-          setDescription(value);
-      } else if (name === "category"){
-          setCategory(value)
-      }
+    const { name, value } = e.target;
+    if (name === "title") {
+      setTitle(value);
+    } else if (name === "description") {
+      setDescription(value);
+    } else if (name === "category") {
+      setCategory(value)
+    }
   };
 
-  const handleImage = (e) => setImage(...e.target.files);
+
+  const handleVideo = (e) => {
+    const file = e.target.files[0];
+    // console.log(file)
+    // const bodyFormData = new FormData();
+    // bodyFormData.append('videoPosts', file);
+    // setVideo(bodyFormData);
+    setVideo(file);
+
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!title || !description || !image) return;
-    const imageUrl = await uploadImage(image);
-    console.log(imageUrl);
-    const videoUrl = await uploadVideo(video);
-    console.log(videoUrl);
-    const post = {
-      userid: user.id,
-      title: title,
-      description: description,
-      image: "",
-      video: "",
-      category: category
-    };
-    const res = await createPost(post);
-    console.log(res)
-  
-  };
 
+    if (isAuthenticated) {
+      let imageUrl = image
+      let videoUrl = video
+      imageUrl = image !== null ? await uploadImage(image[0].file) : null;
+      videoUrl = video !== null ? await uploadVideo(video) : null;
+
+      const post = {
+        userid: user.id,
+        title: title,
+        description: description,
+        image: imageUrl,
+        video: videoUrl,
+        category: category
+      };
+      const {data} = await createPost(post);
+      console.log(data);
+      if (data.ok === true) {
+        console.log(data.msg)
+        setOpen(false)
+      } else {
+        console.log(data.msg)
+      }
+    }
+  }
 
 
   return (
@@ -145,35 +163,46 @@ export default function CreatePost() {
                 <FilePond
                   files={image}
                   onupdatefiles={setImage}
-                  allowMultiple={true}
-                  maxFiles={5}
-                  server="/api"
-                  name="files" 
+                  allowMultiple={false}
+                  maxFiles={1}
+                  name="imagePost" 
                   labelIdle= "Elige o arrastra tus imágenes"
                   className='imageInput'
                 />
               </div>
-              <div className='uploadVideo'>
-              <label htmlFor='fileidVideo'>Elegí tu video</label>
-                <input 
-                  className="videoInput"
-                  id="fileidVideo"
-                  name="video"
-                  type="file"
-                  accept="video/*"
-                  onChange={handleImage}
-                  required
-                  style={{display:'none'}}
+              {/*<div className='uploadVideo'>
+                <FilePond
+                  files={video}
+                  onupdatefiles={setVideo}
+                  allowMultiple={false}
+                  maxFiles={1}
+                  name="videoPost" 
+                  labelIdle= "Elige o arrastra tu video"
+                  className='videoInput'
+                  acceptedFileTypes={['video/*']}
+                /> */}
+
+            <div className='uploadVideo'>
+              <input
+                className="videoInput"
+                id="fileid"
+                placeholder='Puedes sumar videos'
+                name="video"
+                type="file"
+                accept="video/*"
+                onChange={handleChange}
+                required
               />
               <div className='confirmVideo'>
                 {videoUrl && (
-                  <img src={videoUrl} alt="Algo" />
+                  <img alt='' src={videoUrl} />
                 )}
-                </div>
               </div>
             </div>
+          
+          </div>
 
-            <button className="createPostButton" type="submit">
+            <button className="createPostButton" type="submit" onClick={handleSubmit}>
                 Publicar <svg height='25' width='25' fill='#fff' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M96.06 288.3H351.9L252.6 493.8C250.1 499.2 246 503.8 240.1 507.1C235.9 510.3 230 512 224 512C217.1 512 212.1 510.3 207 507.1C201.1 503.8 197.9 499.2 195.4 493.8L96.06 288.3zM386.3 164C392.1 166.4 397.4 169.9 401.9 174.4C406.3 178.8 409.9 184.1 412.3 189.9C414.7 195.7 415.1 201.1 416 208.3C416 214.5 414.8 220.8 412.4 226.6C409.1 232.4 406.5 237.7 402 242.2C397.6 246.6 392.3 250.2 386.5 252.6C380.7 255 374.4 256.3 368.1 256.3H79.88C67.16 256.3 54.96 251.2 45.98 242.2C37 233.2 31.97 220.1 32 208.3C32.03 195.5 37.1 183.4 46.12 174.4C55.14 165.4 67.35 160.4 80.07 160.4H81.06C80.4 154.9 80.06 149.4 80.04 143.8C80.04 105.7 95.2 69.11 122.2 42.13C149.2 15.15 185.8 0 223.1 0C262.1 0 298.7 15.15 325.7 42.13C352.7 69.11 367.9 105.7 367.9 143.8C367.9 149.4 367.6 154.9 366.9 160.4H367.9C374.2 160.4 380.5 161.6 386.3 164z"/></svg></button>
         </div>
       </form>
--- src/components/feedCard/FeedCard.js
+++ src/components/feedCard/FeedCard.js
@@ -15,6 +15,7 @@ import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
+import Share from "../Share/Share";
 
 const element = <FontAwesomeIcon icon={faCoffee} />
 
@@ -92,13 +93,9 @@ export default function FeedCard({ post }) {
                         <button className="socialRatingB" >
                             <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M232 464h-40.01v-117.3c68.52-15.88 118-79.86 111.4-154.1L287.5 14.5C286.8 6.25 279.9 0 271.8 0H48.23C40.1 0 33.22 6.25 32.47 14.5L16.6 192.6c-6.625 74.25 42.88 138.2 111.4 154.2V464H87.98c-22.13 0-40.01 17.88-40.01 40c0 4.375 3.625 8 8.002 8h208c4.377 0 8.002-3.625 8.002-8C272 481.9 254.1 464 232 464zM180.4 300.2c-13.64 3.16-27.84 3.148-41.48-.0371C91.88 289.2 60.09 245.2 64.38 197.1L77.7 48h164.6L255.6 197.2c4.279 48.01-27.5 91.93-74.46 102.8L180.4 300.2z" /></svg>
                         </button>
-                        <button className="socialIcon" >
-                            <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M512 32H120c-13.25 0-24 10.75-24 24L96.01 288c0 53 43 96 96 96h192C437 384 480 341 480 288h32c70.63 0 128-57.38 128-128S582.6 32 512 32zM512 224h-32V96h32c35.25 0 64 28.75 64 64S547.3 224 512 224zM560 416h-544C7.164 416 0 423.2 0 432C0 458.5 21.49 480 48 480h480c26.51 0 48-21.49 48-48C576 423.2 568.8 416 560 416z" /></svg>
-                            COMPARTIR
-                        </button>
+                            <Share />
                         <button className="socialIcon">
                             <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M384 0H96C60.65 0 32 28.65 32 64v384c0 35.35 28.65 64 64 64h288c35.35 0 64-28.65 64-64V64C448 28.65 419.3 0 384 0zM240 128c35.35 0 64 28.65 64 64s-28.65 64-64 64c-35.34 0-64-28.65-64-64S204.7 128 240 128zM336 384h-192C135.2 384 128 376.8 128 368C128 323.8 163.8 288 208 288h64c44.18 0 80 35.82 80 80C352 376.8 344.8 384 336 384zM496 64H480v96h16C504.8 160 512 152.8 512 144v-64C512 71.16 504.8 64 496 64zM496 192H480v96h16C504.8 288 512 280.8 512 272v-64C512 199.2 504.8 192 496 192zM496 320H480v96h16c8.836 0 16-7.164 16-16v-64C512 327.2 504.8 320 496 320z" /></svg>
-
                             GUARDAR
                         </button>
                         <button className="socialIcon">
