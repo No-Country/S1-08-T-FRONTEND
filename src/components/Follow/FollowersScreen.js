33225fed06075a39e51266dd5f7ac0c818b454ef
oliveravictor <ovictoriox@gmail.com>
2022-04-19 17:41:21 -0300

    utilities for follow

--- src/components/Follow/FollowersScreen.js
+++ src/components/Follow/FollowersScreen.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import {useGetFollowersQuery} from "../../app/services/followers";
 import {useParams} from "react-router-dom";
@@ -6,7 +5,7 @@ import CardFollow from "./CardFollow";
 
  const FollowersScreen = () => {
   const {id} = useParams();
-  const {data} = useGetFollowersQuery(id);
+  const {data, error, isLoading, isSuccess, isError, refetch} = useGetFollowersQuery(id);
   const [followers, setFollowers] = useState([]);
 
   useEffect(() => {
@@ -15,18 +14,49 @@ import CardFollow from "./CardFollow";
     }
   },[data]);
 
+  useEffect(() => {
+    refetch();
+  }, []); 
+
   return (
     <>
-     <h2 style={{margin: "1rem 0 0 2rem", color: "#EC5853"}}>Seguidores</h2>
-       {followers.map((follower) => (
-        <CardFollow
-          key={follower.followerID}
-          follower={follower}
-          linkId={follower.followerID}
-        />
-      ))}       
+      <h2 style={{margin: "4rem 0 0 2rem", color: "#EC5853", fontSize: "2rem"}}>Seguidores</h2>
+      {isLoading && <h1 style={{textAlign: "center"}}>Cargando..</h1>}
+      {isError && error.message}
+      {isSuccess &&
+        followers &&
+        followers.map((follower) => (
+          <CardFollow
+            key={follower.followerID}
+            follower={follower}
+            linkId={follower.followerID}
+          />
+        ))}
     </>
   )
 }
+
+// return (
+//   <>
+//    <h2 style={{margin: "1rem 0 0 2rem", color: "#EC5853"}}>Seguidores</h2>
+    
+//    {isLoading && "cargando..."}
+
+//    {isError && error.message}
+
+
+//      {isSuccess &&
+//       followers &&
+//      followers.map((follower) => (
+//       <CardFollow
+//         key={follower.followerID}
+//         follower={follower}
+//         linkId={follower.followerID}
+//       />
+//     ))}       
+//   </>
+// )
+// }
+
 
 export default FollowersScreen;
--- src/components/Follow/FollowingScreen.js
+++ src/components/Follow/FollowingScreen.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import {useGetFollowingQuery} from "../../app/services/followers";
 import {useParams} from "react-router-dom";
@@ -6,7 +5,7 @@ import CardFollow from "./CardFollow";
 
 const FollowingScreen = () => {
   const {id} = useParams();
-  const {data} = useGetFollowingQuery(id);
+  const {data, error, isLoading, isSuccess, isError, refetch} = useGetFollowingQuery(id);
   const [followers, setFollowers] = useState([]);
 
   useEffect(() => {
@@ -15,17 +14,25 @@ const FollowingScreen = () => {
     }
   }, [data]);
 
+  useEffect(() => {
+    refetch();
+  }, []); 
+
   return (
-    <div>
-      <h2 style={{margin: "1rem 0 0 2rem", color: "#EC5853"}}>Seguidos</h2>
-      {followers.map((follower) => (
-        <CardFollow
-          key={follower.followerID}
-          follower={follower}
-          linkId={follower.userid}
-        />
-      ))}
-    </div>
+    <>
+      <h2 style={{margin: "4rem 0 0 2rem", color: "#EC5853", fontSize: "2rem"}}>Seguidos</h2>
+      {isLoading && <h1 style={{textAlign: "center"}}>Cargando..</h1>}
+      {isError && error.message}
+      {isSuccess &&
+        followers &&
+        followers.map((follower) => (
+          <CardFollow
+            key={follower.followerID}
+            follower={follower}
+            linkId={follower.followerID}
+          />
+        ))}
+    </>
   )
 }
 
