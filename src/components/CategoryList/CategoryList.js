67cd857ed36c969c03b2c6c75a61563f09d39419
Cristian Palomeque <cristianpalocseres@gmail.com>
2022-04-20 08:39:44 -0500

    start logic list category component

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useGetAllCategoriesQuery } from '../../app/services/categories';
 
-function CategoryList () {
-  return <div>CategoryList</div>
+function CategoryList() {
+
+  const { data, error, isLoading, isSuccess, isError, refetch } = useGetAllCategoriesQuery();
+
+  const [categories, setcategories] = useState([]);
+
+
+  useEffect(() => {
+    if (data) {
+      setcategories(data);
+    }
+  }, [data]); // eslint-disable-line
+
+  useEffect(() => {
+    refetch();
+  }, []); // eslint-disable-line
+
+  return (
+    <div>
+      <div>CategoryList</div>
+
+      <div>
+        {isLoading && "cargando..."}
+
+        {isError && error.message}
+
+        {isSuccess &&
+          categories &&
+          categories.map((category) => (
+            <div key={category.id}>
+              {category.name}
+            </div>
+          ))}
+      </div>
+    </div>
+  )
+
 }
 
 export default CategoryList
