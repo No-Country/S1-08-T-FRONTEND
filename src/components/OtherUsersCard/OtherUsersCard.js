00a60e801dfd8a27859ec97355a343643abf4403
Cristian Palomeque <cristianpalocseres@gmail.com>
2022-04-20 14:13:03 -0500

    fix styles generals Home, feed, feedCard

--- src/components/OtherUsersCard/OtherUsersCard.js
+++ src/components/OtherUsersCard/OtherUsersCard.js
@@ -11,11 +11,13 @@ export default function OtherUsersCard({
         <div className='otherAvatar'>
             <img alt='avatar' src={avatarDefault} />
         </div>
+      <div className='infoAndSign'>
         <div className='userInfo'>
             <h4 className='otherName'>{username}</h4>
             <p className='otherSubtitle'>{description}</p>    
         </div>
-        <img alt='open' className='opensign' src={open}/>
+        <img alt='open' className='openSign' src={open}/>
+    </div>
     </div>
   );
 }
