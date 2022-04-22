dd6e96bba6951c3e493cdaeeaffe5fe811b6e3d9
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2022-04-22 14:15:12 -0300

    commit mauri changes register
    Signed-off-by: MauricioRaulFerreyra <mauriciotauro82@hotmail.com>

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,19 +6,18 @@ import fontbg from '../../Assets/images/pizzaA.jpg'
 import toast from 'react-hot-toast'
 import { Link, useNavigate } from 'react-router-dom'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
-import { Input } from '@material-ui/core'
 
 const Register = () => {
+  const [registerexitoso, setRegisterexitoso] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [repassword, setRepassword] = useState('')
   const [username, setUsername] = useState('')
   const [nickname, setNickname] = useState('')
   const [register] = useRegisterMutation()
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
 
-  const handleSubmit = async e => {
+  const handleButton = async e => {
     e.preventDefault()
 
     const registerToast = toast.loading('Registrando...')
@@ -27,6 +26,7 @@ const Register = () => {
     const response = await register({
       email,
       password,
+      repassword: password,
       username,
       nickname
     })
@@ -41,27 +41,6 @@ const Register = () => {
     }
   }
 
-  const handleChange = e => {
-    const { name, value } = e.target
-    if (name === 'email') {
-      setEmail(value)
-    } else if (name === 'password') {
-      setPassword(value)
-    } else if (name === 'repassword') {
-      setRepassword(value)
-    } else if (name === 'username') {
-      setUsername(value)
-    } else if (name === 'nickname') {
-      setNickname(value)
-    }
-  }
-
-  // mauri debes hacer las validaciones necesarias del formulario
-
-  if (password !== repassword) {
-    console.log('Los campos deben coincidir')
-  }
-
   return (
     <>
       <Formik
@@ -72,127 +51,138 @@ const Register = () => {
           email: '',
           password: ''
         }}
-        validate={value => {
+        validate={valores => {
           const errors = {}
-          if (!value.username) {
+          if (!valores.username) {
             errors.username = 'Nombre requerido'
+          } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.username)) {
+            errors.username = 'Nombre solo puede contener letras y espacios'
           }
-          if (!value.nickname) {
+          if (!valores.nickname) {
             errors.nickname = 'Nombre de cuenta requerido'
+          } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nickname)) {
+            errors.nickname =
+              'Nombre de cuenta solo puede contener letras y espacios'
           }
-          if (!value.repassword) {
+          if (!valores.repassword) {
             errors.repassword = 'Confirmar contraseña'
+          } else if (valores.password !== valores.repassword) {
+            errors.repassword = 'Las contraseñas deben coincidir'
           }
-          if (!value.email) {
+          if (!valores.email) {
             errors.email = 'Email requerido'
           } else {
-            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value.email)) {
+            if (
+              !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+                valores.email
+              )
+            ) {
               errors.email = 'email invalido'
             }
           }
-          if (!value.password) {
+          if (!valores.password) {
             errors.password = 'Contraseña requerida'
           }
           return errors
         }}
-        handleChange={handleChange}
+        onSubmit={(e, valores, { resetForm }) => {
+          e.preventDefault()
+          resetForm()
+          console.log(valores)
+          setRegisterexitoso(true)
+          setTimeout(() => {
+            setRegisterexitoso(false)
+          }, 3000)
+        }}
       >
-        <div className='register-container'>
-          <div className='register-img'>
-            <img alt='' src={fontbg} />
-          </div>
-          <div className='register-div'>
-            <p className='register-title'>Registrate</p>
-            <Form className='register-form'>
-              <div className='register-form-control'>
-                <Field
-                  type='text'
-                  name='email'
-                  placeholder='Email'
-                  //onChange={handleChange}
-                  required
-                />
-                <ErrorMessage
-                  className='errorMessage'
-                  name='email'
-                  component='div'
-                />
-              </div>
-              <div className='register-form-control'>
-                <Field
-                  type='text'
-                  placeholder='Nombre'
-                  name='username'
-                  //onChange={handleChange}
-                  required
-                />
-                <ErrorMessage
-                  className='errorMessage'
-                  name='username'
-                  component='div'
-                />
-              </div>
-              <div className='register-form-control'>
-                <Field
-                  type='text'
-                  placeholder='Nombre de Cuenta'
-                  name='nickname'
-                  //onChange={handleChange}
-                  required
-                />
-                <ErrorMessage
-                  className='errorMessage'
-                  name='nickname'
-                  component='div'
-                />
-              </div>
-              <div className='register-form-control'>
-                <Field
-                  type='password'
-                  placeholder='Contraseña'
-                  name='password'
-                  //onChange={handleChange}
-                  required
-                />
-                <ErrorMessage
-                  className='errorMessage'
-                  name='password'
-                  component='div'
-                />
-              </div>
-              <div className='register-form-control'>
-                <Field
-                  type='password'
-                  placeholder='Confirmar contraseña'
-                  name='repassword'
-                  //onChange={handleChange}
-                  required
-                />
-                <ErrorMessage
-                  className='errorMessage'
-                  name='repassword'
-                  component='div'
-                />
-              </div>
-              <div className='register-button-container'>
-                <button
-                  className='register-button'
-                  type='submit'
-                  onClick={handleSubmit}
-                  disabled={loading}
-                >
-                  Registrarse
-                </button>
-              </div>
+        {({ errors }) => (
+          <div className='register-container'>
+            <div className='register-img'>
+              <img alt='' src={fontbg} />
+            </div>
+            <div className='register-div'>
+              <p className='register-title'>Registrate</p>
+              <Form className='register-form'>
+                <div className='register-form-control'>
+                  <Field type='email' name='email' placeholder='Email' />
+                  <ErrorMessage
+                    name='email'
+                    component={() => (
+                      <div className='errorMessage'>{errors.email}</div>
+                    )}
+                  />
+                </div>
+                <div className='register-form-control'>
+                  <Field type='text' placeholder='Nombre' name='username' />
+                  <ErrorMessage
+                    name='username'
+                    component={() => (
+                      <div className='errorMessage'>{errors.username}</div>
+                    )}
+                  />
+                </div>
+                <div className='register-form-control'>
+                  <Field
+                    type='text'
+                    placeholder='Nombre de Cuenta'
+                    name='nickname'
+                  />
+                  <ErrorMessage
+                    name='nickname'
+                    component={() => (
+                      <div className='errorMessage'>{errors.nickname}</div>
+                    )}
+                  />
+                </div>
+                <div className='register-form-control'>
+                  <Field
+                    type='password'
+                    placeholder='Contraseña'
+                    name='password'
+                  />
+                  <ErrorMessage
+                    name='password'
+                    component={() => (
+                      <div className='errorMessage'>{errors.password}</div>
+                    )}
+                  />
+                </div>
+                <div className='register-form-control'>
+                  <Field
+                    type='password'
+                    placeholder='Confirmar contraseña'
+                    name='repassword'
+                  />
+                  <ErrorMessage
+                    name='repassword'
+                    component={() => (
+                      <div className='errorMessage'>{errors.repassword}</div>
+                    )}
+                  />
+                </div>
+                <div className='register-button-container'>
+                  <button
+                    className='register-button'
+                    type='submit'
+                    onClick={handleButton}
+                    disabled={loading}
+                  >
+                    Registrarse
+                  </button>
+                  {registerexitoso && (
+                    <p className='registerExitoso'>Registro Exitoso</p>
+                  )}
+                </div>
 
-              <div className='register-form-control'>
-                <h6>
-                  ¿ Ya tienes cuenta ?<Link to='/login'> Iniciar sesion</Link>
-                </h6>
-              </div>
-            </Form>
+                <div className='register-form-control'>
+                  <h6>
+                    ¿ Ya tienes cuenta ?<Link to='/login'> Iniciar sesion</Link>
+                  </h6>
+                </div>
+              </Form>
+            </div>
           </div>
-        </div>
+        )}
       </Formik>
     </>
   )
