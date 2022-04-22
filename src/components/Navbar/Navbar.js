c85dd4e7300fa154878e631f471578100671cb76
Cristian Palomeque <72889927+ccpc93js@users.noreply.github.com>
2022-04-22 09:42:26 -0500

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -66,7 +66,7 @@ export default function Navbar() {
                   )}
                   <CreatePost />
                   <Notifications />
-                  <Link to={`/${user.id}/me`}>
+                  <Link to={`/me/${user.id}`}>
                     <IconButton tooltip="Perfil" flow="down">
                       <Avatar
                         sx={{
